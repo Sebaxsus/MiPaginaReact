57ff5b6ac539622f8fbe2a688a8d935a3fefd2c6
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2024-10-02 01:43:46 -0500

    ver 0.0.2 | Agregado estado al componente Manga para cambiar su estilo y otro estado para cambiar la descripcion dentro del Componente Manga, Agregada la propiedad desc al componente Manga para enviarle un array con dos descripciones y cambiarlas segun el nombre de clase.

--- src/app.jsx
+++ src/app.jsx
@@ -6,10 +6,39 @@ import { NavBar } from './components/navBar/navBar.jsx'
 import { useState } from 'react'
 
 //Componente Manga a Renderizar
-const Manga = ({ children, estado, index}) => {
+const Manga = ({ children, estado, index, desc}) => {
+
+    {/*Asi se hace un comentario dentro de un componente react */}
+
+    {/*
+        Creo un Estado para cambiar la clase del componente
+        Para Cambiar sus estilos
+     */
+    }
+    // el Metodo useState me devuelve un array de dos posiciones
+    // La posicion 0 es el valor,se puede dar un valor incial inicializando -el que esta en ()-
+    // Esta inicializacion solo ocurrira una vez cuando se renderice el componente
+    // La posicion 1 es la funcion para modificar el valor que se guarda
+    const [ClassName, setClassName] = useState('Manga')
+    const [Description, setDescription] = useState(desc[0])
+
+    //Aqui lo uso ClassName como Bool y si es true lo ponga en manga
+    //Si es False lo ponga en Manga-View
+    //const style = ClassName ? 'Manga' : 'Manga-View'
+
     return (
-        <div className='Manga'>
+        <div className={ClassName} onClick={() => {
+            {/* 
+                Aqui verifico que si el Nombre en ClassName
+                es Igual a 'Manga' lo cambie a 'Manga-View'
+                Y si no es 'Manga' lo cambie a 'Manga'
+             */
+            }
+            setClassName(ClassName === 'Manga' ? 'Manga-View': 'Manga')
+            setDescription(ClassName === 'Manga' ? desc[1]: desc[0])
+        }}>
             {children}
+            <p>{Description}</p>
         </div>
     )
 }
@@ -25,11 +54,11 @@ const Manga = ({ children, estado, index}) => {
 
 export function App() {
     // Array de Mangas
-    const [mangaList, setMangaList] = useState([
-        {title: "Manga", desc: "Bueno", img: './Eula.jpg'},
-        {title: "Manga", desc: "Bueno", img: './Eula.jpg'},
-        {title: "Manga", desc: "Bueno", img: './Eula.jpg'}
-    ])
+    const mangaList = [
+        {title: "Manga", desc: ["Bueno", "Un Anime Completo con Trama de..."], img: './Eula.jpg'},
+        {title: "Manga", desc: ["Bueno", "Un Anime Completo con Trama de..."], img: './Eula.jpg'},
+        {title: "Manga", desc: ["Bueno", "Un Anime Completo con Trama de..."], img: './Eula.jpg'}
+    ]
 
     return (
         <>
@@ -44,9 +73,9 @@ export function App() {
                                 <Manga
                                     key={index}
                                     index={index}
+                                    desc={mangaList[index].desc}
                                 >
                                     <h1>{mangaList[index].title}</h1>
-                                    <p>{mangaList[index].desc}</p>
                                     <img src='/Eula.jpg' alt={mangaList[index].img}></img>
                                 </Manga>
                             )
