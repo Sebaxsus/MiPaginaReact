4f6e1be6cf113078280bddd83f953c8e8d9e6b7c
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-03-27 00:22:19 -0500

    Changed API to Use the data from the API REST Knowing it's send filtred From MySQL and the BACK | ADDED deleteManga, updateManga Methods to the API |

--- src/services/api.js
+++ src/services/api.js
@@ -5,19 +5,19 @@ const BASE_URL = "http://localhost:3000"
 export const getMangas = async (title) => {
     try {
         const res = await axios.get(`${BASE_URL}/mangas`)
-        const data = res.data.filter((manga) => {
-            if (manga.title.toLowerCase().includes(title)) return manga
-        })
-
+        // const data = res.data.filter((manga) => {
+        //     if (manga.title.toLowerCase().includes(title)) return manga
+        // })
+        const data = res.data
         return data
     } catch (err) {
         console.error("Fallo el get Mangas: ", err)
-        throw err
+        return err
     }
 }
 
 export const  postManga = async (body = {}) => {
-    console.log("data Post Manga: ", body)
+    //console.log("data Post Manga: ", body)
     try {
         const res = await axios.post(`${BASE_URL}/mangas`, body, {
             headers: {
@@ -27,8 +27,30 @@ export const  postManga = async (body = {}) => {
         return res
     } catch (err) {
         console.error(`Fallo el post Manga: `, err)
-        throw err
+        return err
     }
 }
 
-export default { getMangas, postManga}
+export const deleteManga = async ({ id }) => {
+    console.log(id)
+    try {
+        const res = await axios.delete(`${BASE_URL}/mangas/${id}`)
+        return res
+    } catch (err) {
+        console.error("Fallo el delete Manga: ", err)
+        return err
+    }
+}
+
+export const updateManga = async ({ id, body}) => {
+    console.log(id, body)
+    try {
+        const res = await axios.patch(`${BASE_URL}/mangas/${id}`)
+        return res
+    } catch (err) {
+        console.error("Fallo el PATCH Manga: ", err)
+        return err
+    }
+}
+
+export default { getMangas, postManga, deleteManga, updateManga }
