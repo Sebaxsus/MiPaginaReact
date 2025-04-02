6d6b1349b404c02e8036948a72eb4984ca767ca4
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-04-02 08:26:00 -0500

    Added fetch for every render on View | Added dynamic Route Params to Fetch the data for the specific Anime/Manga | Fixed Style (Work in Progess)

--- src/pages/View/View.jsx
+++ src/pages/View/View.jsx
@@ -1,14 +1,60 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'wouter'
+
 import './View.css'
 
+import { mangasController, animesController } from '../../services/newApi'
+
 export function View() {
+    const [data, setData ] = useState({})
+    const [loading, setLoading] = useState(true)
+    const routeParams = useParams()
+    useEffect(() => {
+        setLoading(true)
+        const controller = routeParams.type === "Manga" ? mangasController : animesController
+        
+        controller.getById({id: routeParams.id }).then( data => {
+            // console.log("View data: ",data)
+            setData(data)
+        }).catch( e => {
+            console.error("Error Obteniendo la informacion: ", e) 
+        })
+        .finally( () => {
+            setLoading(false)
+        })
+
+    }, [routeParams.id,routeParams.type])
+
+
+    if (loading) {
+        return (
+            <h2>Loading...</h2>
+        )
+    }
+
     return (
         <article className="view-Container">
             <header className="view-Header">
-                <h1>Titulo</h1>
-                <img src="./Eula.jpg" alt='Imgagen de portada del Manga Titulo'/>
+                <h1 className='titulo'>
+                    {data.title}
+                </h1>
             </header>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor nisi sapiente ab ipsa porro commodi ipsam, quo labore consequatur numquam, delectus at, quasi blanditiis enim nobis alias doloribus voluptatibus quaerat.</p>
+            <img src={data.img.startsWith('.') ? data.img.slice(1,) : data.img} alt={`Imgagen de portada del ${routeParams.type} ${data.title}`}/>
+            <p className='descripcion'>
+                {data.description}
+            </p>
+            <ul className='generos'>
+                {data.genre.map((genero, index) => {
+                    return (
+                        <li 
+                            key={index}
+                            className="generosItem"
+                        >
+                            {genero}
+                        </li>
+                    )
+                })}
+            </ul>
         </article>
     )
 }
