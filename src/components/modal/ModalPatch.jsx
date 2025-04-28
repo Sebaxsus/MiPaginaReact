120a958b3eacbdcda8e31f0d1bb7313848dc36f3
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-04-28 14:44:42 -0500

    ### feat: Cambio de altura modal dependiendo de la resolucion - Se añadio un condicional utilizando la API nativa Window para verificar el alto de la ventana (Navegador) y establecer el valor de una variable global de css - Se añadio un detector de eventos usando la API Nativa Window con el evento **resize** para cambiar el valor de una variable global de css y con ello ajustar el alto del modal - Se añadio un debouncer para controlar la ejecucion del codigo dentro del event listener. ## Refactor: Se migra el uso de clases TailwindCSS a un modulo CSS - Se importa el modulo CSS al Componenete - Se migra todos los estilos repetitivos al Modulo CSS

--- src/components/modal/ModalPatch.jsx
+++ src/components/modal/ModalPatch.jsx
@@ -5,10 +5,12 @@ import { mangasController } from "../../services/newApi"
 import { animesController } from "../../services/newApi"
 import { useMainContext } from "../../Context"
 
+import "./modal.css"
+
 function campoForm({ genero, data = [] }) {
     // console.log(genero.name, data, data.find((g) => {return g.id === genero.id}) ? true : false)
     return (
-        <label className="flex gap-2">
+        <label>
             <input type="checkbox" name="generos" id={genero.id} value={genero.id} defaultChecked={data.find((g) => {return g.id === genero.id}) ? true : false}/>
             {genero.name}
         </label>
@@ -32,6 +34,109 @@ export default function Modal(props) {
 
     const titulo = routeParams.type
     const controller = routeParams.type === "Mangas" ? mangasController : animesController
+
+    /*
+    Creando un eventListener para verificar los cambios de
+    Ancho y alto (resolusion) de la ventana del navegador
+    
+    Paro esto uso el evento del resize de la API nativa del
+    navegador Window, Este evento se activa en el momento que la
+    resolusion de la ventana cambia
+
+    Para evitar problemas de rendimiento con el evento se utilizan
+    dos tecnicas Throttling (Embotellamiento) y
+    Debouncing (No se que es jaja).
+
+    Throttling se encarga de "Embotellar" bloquear la ejecucion
+    cada determinado tiempo
+
+
+    Debouncing se encarga de esperar a que la ventana deje de cambiar de
+    tamaño (Activar el evento) para despues ejecutar el codigo.
+
+    ----------------- De aqui para abajo es un copy paste de la documentacion de MDN generado con Copilot ------------------
+    // https://developer.mozilla.org/es/docs/Web/API/Window/resize_event
+    // https://bencentra.com/code/2015/02/27/optimizing-window-resize.html
+    // https://css-tricks.com/debouncing-throttling-explained-examples/
+    // https://remysharp.com/2010/07/21/throttling-function-calls
+    //  https://es.linkedin.com/posts/midudev_crea-una-funci%C3%B3n-debounce-en-react-en-s%C3%B3lo-activity-7048968258250526720-AEqR#:~:text=El%20%22debouncing%22%20es%20una%20t%C3%A9cnica%20que%20se,la%20actualizaci%C3%B3n%20del%20estado%20en%20un%20componente.&text=En%20lugar%20de%20eso%2C%20con%20el%20debounce%2C,por%20si%20el%20usuario%20vuelve%20a%20escribir.
+
+    El Debouncing y el Throttling son tecnicas de optimizacion
+    de eventos que se utilizan para limitar la cantidad de veces que
+    se ejecuta una funcion en respuesta a un evento.
+
+    Ambas tecnicas son utiles para mejorar el rendimiento de las
+    aplicaciones web, especialmente cuando se trata de eventos
+    que se activan con frecuencia, como el scroll o el resize de la
+    ventana.
+
+    La diferencia entre ambas tecnicas es la siguiente:
+
+    Debouncing es reiniciar el timeout cada vez que se activa el evento
+    y si no se activa el evento en el tiempo determinado se ejecuta el
+    codigo. En este caso el timeout es de 100ms, por lo que si el evento
+    se activa cada 100ms, el timeout se reinicia y no se ejecuta el
+    codigo. Si el evento no se activa en 100ms, se ejecuta el codigo.
+
+    Throttling es ejecutar el evento cada determinado tiempo
+
+    Throttiling es mas util para eventos que se activan constantemente
+    como el scroll, resize, etc. En este caso no es necesario
+    ya que el evento resize no se activa constantemente, sino cada vez que
+    sino cada vez que se cambia el tamaño de la ventana.
+    En este caso no es necesario, pero lo dejo para aprender como se hace
+    un debounce
+    y un throttle.
+    */
+    function debounce(fn, delay) {
+        // Variable declarada con let
+        // Para asegurar su uso exclusivo dentro del alcance
+        // de la funcion (funtion scope)
+        let timer = null
+
+        // devuelvo una funcion para que se ejecute 
+        // dentro del event listener
+        return function () {
+            let context = this
+            let args = arguments
+            // Limpio el timeout si existe para
+            // Reiniciarlo (Crear uno nuevo con el mismo delay)
+            clearTimeout(timer)
+
+            // Creo el timeout (Throttle Function)
+            // Que le aplica a la funcion creada en el event listener
+            // el contexto y los argumentos de la funcion
+            // que se ejecuta dentro del event listener
+            // Esto es para que la funcion no se ejecute cada vez que
+            // se activa el evento resize
+            // sino cada vez que se deja de activar el evento
+
+            // El contexto es el objeto que se esta ejecutando
+            // y los argumentos son los argumentos que se le pasan a la funcion
+
+            timer = setTimeout(() => {
+                // console.log("Cambio la resolusion de la ventana 1, Ancho: ", window.innerWidth, " Alto: ", window.innerHeight)
+                fn.apply(context, args)
+            }, delay)
+        }
+    }
+
+    window.addEventListener("resize", debounce(() => {
+        // console.log("Cambio la resolusion de la ventana, Ancho: ", window.innerWidth, " Alto: ", window.innerHeight)
+        window.innerHeight >= 900 
+            ? 
+            document.documentElement.style.setProperty("--modalHeigth", "750px")
+            :
+            document.documentElement.style.setProperty("--modalHeigth", "540px")
+    }, 100))
+
+    // Se encarga de cambiar el alto del modal
+    // dependiendo de la resolusion de la ventana al ejecutar el modal.
+    window.innerHeight >= 900 
+            ? 
+            document.documentElement.style.setProperty("--modalHeigth", "750px")
+            :
+            document.documentElement.style.setProperty("--modalHeigth", "540px")
 
     const validateFormText = (event, place = '') => {
         const element = document.getElementById(event.target.id)
@@ -111,14 +216,15 @@ export default function Modal(props) {
     
         }
 
+        
     return (
         <>
-            <article className="sticky top-20 z-[1] mx-[10px] justify-items-center">
-                <article className="absolute bg-gray-900 p-5 rounded-2xl [box-shadow:7px_2px_18px_6px_rgba(3,50,51)] flex aspect-[2/1] flex-col items-center w-1/2 left-[24%] mt-2">
-                    <button onClick={props.onClose} className="absolute self-end pr-2 border-0 bg-transparent text-[20px] cursor-pointer" >✖</button>
-                    <h2 className="font-bold underline underline-offset-2 decoration-[#2fadcc] text-xl pb-2">Modificar un {titulo}!</h2>
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-y-4 justify-center w-4/5">
-                        <label className="flex flex-col gap-y-4">
+            <article className="modalPatch-Container ">
+                <article className="modalPatch ">
+                    <button onClick={props.onClose} className="modalPatch-CloseBtn absolute self-end pr-2 border-0 bg-transparent text-[20px] cursor-pointer" >✖</button>
+                    <h2>Modificar un {titulo}!</h2>
+                    <form onSubmit={handleSubmit} className="modalPatch-Form ">
+                        <label>
                             <h2>Titulo</h2>
                             <input
                                 type="text"
@@ -129,10 +235,10 @@ export default function Modal(props) {
                                 onChange={(e) => {validateFormText(e, "Titulo");setFormTitle(e.target.value)}}
                                 // onKeyDown={(e) => {}}
                                 id="modalTitle"
-                                className="focus-visible:outline-0 border-b ml-6 indent-2 py-2"
+                            
                             />
                         </label>
-                        <label className="flex flex-col gap-y-4">
+                        <label>
                             <h2>Descripcion: </h2>
                             <textarea
                                 placeholder={`Decripcion del ${titulo}`}
@@ -142,10 +248,10 @@ export default function Modal(props) {
                                 onChange={(e) => {validateFormText(e, `Decripcion del ${titulo}`);setFormDesc(e.target.value)}}
                                 // onKeyDown={(e) => {}}
                                 id="modalBody"
-                                className="focus-visible:outline-0 border border-gray-300/90 rounded-2xl indent-2 py-2 ml-6"
+                            
                             />
                         </label>
-                        <label className="flex flex-col gap-y-4">
+                        <label>
                             <h2>URL de la Imagen:</h2>
                             <input
                                 type="text"
@@ -157,10 +263,10 @@ export default function Modal(props) {
                                 // Doc de patter en UTILS urlValidator.js                           
                                 pattern="^((https?:\/\/)|www\.)[a-zA-Z0-9\/\-_]{3,192}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?([\w\/\-_?=:;,]+)?(\.(webp|png|jpeg|jpg|gif))?$|^(\/[\w\-.\/]{3,192})$|^(data:image\/(webp|png|jpeg|jpg|gif);base64,[a-zA-Z0-9+\/=]+)$"
                                 id="modalUrl"
-                                className="focus-visible:outline-0 border border-gray-300/90 rounded-xl indent-2 py-2 ml-6"
+                                className="border rounded-xl"
                             />
                         </label>
-                        <label className="flex flex-col gap-y-4">
+                        <label>
                             <h2>Capitulos</h2>
                             <input 
                                 type="number"
@@ -171,10 +277,10 @@ export default function Modal(props) {
                                 value={formChapters}
                                 onChange={(e) => {setChapters(e.target.value)}}
                                 id="modalChapters"
-                                className="focus-visible:outline-0 border border-gray-300/90 rounded-xl indent-2 py-2 ml-6"
+                                className="border rounded-xl"
                             />
                         </label>
-                        <label className="flex flex-col gap-y-4">
+                        <label>
                             <fieldset className="grid grid-cols-3 gap-2">
                                 <legend>Escoja el genero:</legend>
                                 {/* Para generar los generos usar la respuesta de la api seria optimo en mi opinion ya que me asegurario de usar valores que pueda almacenar */}
@@ -188,7 +294,7 @@ export default function Modal(props) {
                         </label>
                         <button
                             type="submit"
-                            className="border rounded-md border-cyan-600 self-center p-1"
+                            className="modalPatch-Form-Btn border rounded-md border-cyan-600 self-center p-1"
                         >
                             Editar
                         </button>
